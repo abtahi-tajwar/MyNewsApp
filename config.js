0b6775bf23fbd77a6e6abf7420c5290dcebda4a1
Abtahi Tajwar <abtahitajwar@gmail.com>
2020-04-13 15:51:59 +0600

    News heading are fetched

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var info = {
     "website": "https://newsapi.orgs/account",
-    "key": "e5c2d1ba225f4ef397ff71e86219785c"
+    "key": "e5c2d1ba225f4ef397ff71e86219785c",
+    "url": "http://newsapi.org/v2/everything?q=bitcoin&from=2020-03-13&sortBy=publishedAt&apiKey="
 }
-export default info;
--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-import info from './config.js';
-
+var news_image = document.querySelector(".news_image");
+var sideNav = document.querySelector(".sidenav");
+console.log(news_image);
+news_image.style.backgroundImage = "url('https://images.cointelegraph.com/images/740_aHR0cHM6Ly9zMy5jb2ludGVsZWdyYXBoLmNvbS9zdG9yYWdlL3VwbG9hZHMvdmlldy9iZmEyOWVmM2RlNWJiYjBmZDQ3Yjc4NGY1NTA1ZDYyYy5qcGc=.jpg')";
 var request = new XMLHttpRequest();
-console.log(info.website);
+var data;
+request.open('GET', 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-03-13&sortBy=publishedAt&apiKey=e5c2d1ba225f4ef397ff71e86219785c', true);
+request.onload = function() {
+    data = JSON.parse(request.responseText).articles;
+    console.log(data);
+    addToNav(data);
+}
+request.send();
+function addToNav(data) {
+    data.forEach(element => {
+        str = `
+        <div class="news">
+            <h1><a href="${element.url}">${element.title} <span>${element.source.name}</span></a></h1>
+            <p>${element.description}</p>
+            <p>Author: ${element.author}</p>
+            <p>Published at: ${element.publishedAt}</p>
+        </div>
+        `;
+        sideNav.innerHTML += str;
+    });
+}
